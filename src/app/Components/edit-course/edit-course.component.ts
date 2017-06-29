17c5f423da1cc6f71da1c54f08d17d1ddcc69561
jarupong pajakgo <lagmanmc@gmail.com>
2017-06-30 01:24:37 +0700

    [Angular,Firebase] add component for edit course

--- src/app/Components/edit-course/edit-course.component.ts
+++ src/app/Components/edit-course/edit-course.component.ts
@@ -1,6 +1,6 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase, FirebaseObjectObservable} from 'angularfire2/database';
+import { AngularFireDatabase} from 'angularfire2/database';
 import {CKEditorComponent} from 'ng2-ckeditor';
 
 @Component({
@@ -9,19 +9,37 @@ import {CKEditorComponent} from 'ng2-ckeditor';
   styleUrls: ['./edit-course.component.css']
 })
 export class EditCourseComponent implements OnInit {
-   private Topic: String;
-   private Description: String;
+   private Topic: String = '';
+   private Description: String = '';
    private Article: String;
-   private DetailCourse: FirebaseObjectObservable<any>;
+   private content: String = '';
   constructor(private edit: AngularFireDatabase , private router: Router) { 
       if(localStorage.getItem('CourseID') != null){
-         this.DetailCourse = edit.object(`/Course/${localStorage.getItem('CourseID')}`);
+          edit.list('/Course',{
+           query:{
+               orderByKey: true,
+               equalTo: localStorage.getItem('CourseID')
+           }
+          }).subscribe((data)=>{
+            this.Topic = data[0].Topic;
+            this.Description = data[0].Description;
+            this.content = data[0].Content;
+          });
       }else{
         this.router.navigate(['/']);
       }
   }
 
   editCourse(){
+    this.edit.list('/Course').update(localStorage.getItem('CourseID'),{
+      Topic: this.Topic,
+      Description: this.Description,
+      Content: this.content
+    }).then((data)=>{
+      console.log('เพิ่มเรียบร้อย');
+    }).catch((err)=>{
+      console.log(err);
+    });
 
 
   }
